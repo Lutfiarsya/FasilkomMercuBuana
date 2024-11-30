09b41327bad42cbf401b9aea8aacf4abcd9779a8
Lutfiarsya <lutfiarsya34@gmail.com>
2024-11-30 23:24:11 +0700

    add nama admin

--- Frontend/src/Component/Navbar.jsx
+++ Frontend/src/Component/Navbar.jsx
@@ -1,6 +1,8 @@
 import LogoMercu from '../Assets/images 1.png'
 import ProfilePicture from '../Assets/download (5).jpg'
+import { useNavigate } from 'react-router-dom'
 const Navbar = () => {
+
     return(
         <nav className='flex flex-row justify-between w-full mt-2'>
             <img
@@ -10,12 +12,15 @@ const Navbar = () => {
             className='ml-4'
             />
             {/* Profile Picture */}
+            <div className='flex flex-row items-center'>
+                <h2 className="mr-6 text-regular font-['Poppins']">Luthfie Arsya</h2>
                 <img
                 src={ProfilePicture}
                 width={50}
                 height={50}
                 className='rounded-full mr-4'
                 />
+            </div>
         </nav>
     )
 }
