c66a63fb6da826ba34271d358813dcb79470d411
Lutfiarsya <lutfiarsya34@gmail.com>
2024-12-04 19:51:14 +0700

    dinamis

--- Frontend/src/Component/TableMahasiswa/Table.jsx
+++ Frontend/src/Component/TableMahasiswa/Table.jsx
@@ -1,67 +1,48 @@
 import { useState } from "react"
+import ActionButton from "../ActionButton";
 
-const Table = ({category, filter}) => {
+const Table = ({category, filter, data, Judul1, Judul2, Judul3, Judul4, Judul5, Judul6}) => {
 
-    const data = {
-        "MBKM": [
-          { 'NIM': '14145141', 'Nama': 'Nama 1', 'Angkatan': '2021', 'DosenPembimbing': 'Dosen 1', 'TanggalUpload': '2023-01-01' },
-          { 'NIM': '14145142', 'Nama': 'Nama 2', 'Angkatan': '2020', 'DosenPembimbing': 'Dosen 2', 'TanggalUpload': '2023-01-02' },
-          { 'NIM': '14145145', 'Nama': 'Nama 3', 'Angkatan': '2020', 'DosenPembimbing': 'Dosen 3', 'TanggalUpload': '2023-01-02' },
-          { 'NIM': '14145159', 'Nama': 'Nama 4', 'Angkatan': '2020', 'DosenPembimbing': 'Dosen 3', 'TanggalUpload': '2023-01-02' },
-          { 'NIM': '14145155', 'Nama': 'Nama 5', 'Angkatan': '2020', 'DosenPembimbing': 'Dosen 3', 'TanggalUpload': '2023-01-02' },
-          { 'NIM': '14145156', 'Nama': 'Nama 6', 'Angkatan': '2020', 'DosenPembimbing': 'Dosen 3', 'TanggalUpload': '2023-01-02' },
-          { 'NIM': '14145157', 'Nama': 'Nama 7', 'Angkatan': '2020', 'DosenPembimbing': 'Dosen 3', 'TanggalUpload': '2023-01-02' },
-          { 'NIM': '14145157', 'Nama': 'Nama 7', 'Angkatan': '2020', 'DosenPembimbing': 'Dosen 3', 'TanggalUpload': '2023-01-02' },
-          { 'NIM': '14145157', 'Nama': 'Nama 7', 'Angkatan': '2020', 'DosenPembimbing': 'Dosen 3', 'TanggalUpload': '2023-01-02' },
-        ],
-        "MPTI": [
-          { "NIM": '14145146', "Nama": 'Nama 3', "Angkatan": '2021', "DosenPembimbing": 'Dosen 3', "TanggalUpload": '2023-01-03' },
-        ],
-        'Tugas Akhir': [
-          { "NIM": '14145149', "Nama": 'Nama 4', "Angkatan": '2022', "DosenPembimbing": 'Dosen 4', "TanggalUpload": '2023-01-04' },
-        ],
-        'Tugas Penelitian': [
-          { "NIM": '14145192', "Nama": 'Nama 5', "Angkatan": '2023', "DosenPembimbing": 'Dosen 5', "TanggalUpload": '2023-01-05' },
-        ],
-      };
 
-const selectedData = data[category]
+const selectedData = category ? data[category] : data
+
 
 const filteredData = selectedData?.filter((items) =>
+  filter ? 
     Object.values(items).some((val) =>
-      String(val).toLowerCase().includes(filter.toLowerCase())
-    )
+      String(val).toLowerCase().includes(filter?.toLowerCase())
+    ) : true
   );
+  console.log(filteredData)  
 
     return(
-        <div className="w-full flex  items-center text-center max-h-[280px] justify-center h-full overflow-y-auto">
+        <div className="relative w-full max-h-[280px] overflow-y-auto border border-gray-300">
             <table className="w-full table-auto border-collapse  text-white text-center font-['Poppins'] shadow-[4px_4px_5px_-4px_black]">
-                <thead className="text-center sticky top-0">
-                    <tr className="bg-[--primary-color] px-2 h-10 ">
-                        <th className="border-x-1 border-white">NIM</th>
-                        <th>Nama</th>
-                        <th>Periode</th>
-                        <th>Dosen Pembimbing</th>
-                        <th>Tanggal Upload</th>
-                        <th>Action</th>
+                <thead>
+                    <tr className="bg-[--primary-color] h-12 sticky top-0 z-10 text-white">
+                        <th className="border-x-1 border-white">{Judul1}</th>
+                        <th>{Judul2}</th>
+                        <th>{Judul3}</th>
+                        <th>{Judul4}</th>
+                        <th>{Judul5}</th>
+                        <th>{Judul6}</th>
                     </tr>
                 </thead>
-                <tbody className="text-center overflow-y-auto">
+                <tbody className="text-center">
                     {filteredData?.map((items, index) => {
                         return(
-                        <tr className="bg-white text-black px-2 h-10" key={index}>
-                            <td>{items.NIM}</td>
+                        <tr key={index} className="bg-white text-black h-12 border-t">
+                            <td>{items.NIM || items.NID}</td>
                             <td>{items.Nama}</td>
-                            <td>{items.Angkatan}</td>
-                            <td>{items.DosenPembimbing}</td>
-                            <td>{items.TanggalUpload}</td>
-                            <td>Coming soon</td>
+                            <td>{items.Angkatan || items.Departemen}</td>
+                            <td>{items.DosenPembimbing || items.Keahlian}</td>
+                            <td>{items.TanggalUpload || null}</td>
+                            <td>{Judul6 ?  <ActionButton/> : null}</td>
                         </tr>
                         )
                     })}
                 </tbody>
             </table>
-
         </div>
     )
 }
