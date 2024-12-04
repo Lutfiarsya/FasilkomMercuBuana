7a84822a24a5262705afd23b5e900360836cdfa9
Lutfiarsya <lutfiarsya34@gmail.com>
2024-12-04 19:50:51 +0700

    tinggal styling

---
+++ Frontend/src/pages/Profile.jsx
@@ -0,0 +1,16 @@
+const Profile = () => {
+    return(
+        <div>
+            <div>
+                <p>Ini Navbar</p>
+            </div>
+            <div>
+                <img/>
+                <h1>Luthfie Arsya Darmaputra</h1>
+                <p>41524010185@student.mercubuana.ac.id</p>
+            </div>
+        </div>
+    )
+}
+
+export default Profile
