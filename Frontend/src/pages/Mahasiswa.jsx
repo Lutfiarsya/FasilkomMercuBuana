9d743ac47b44d6987b3d847f1157966cbc761ab8
Lutfiarsya <lutfiarsya34@gmail.com>
2024-12-05 22:22:55 +0700

    Revisi layout

--- Frontend/src/pages/Mahasiswa.jsx
+++ Frontend/src/pages/Mahasiswa.jsx
@@ -1,40 +1,49 @@
 import Navbar from "../Component/Navbar"
-import MPTI from '../Assets/completed-task 3.png'
-import MBKM from '../Assets/completed-task 1.png'
-import TA from '../Assets/completed-task 2.png'
-import TP from '../Assets/resume_8468345.png'
+import KP from '../Assets/Icons Services/task-management (1).png'
+import MBKM from '../Assets/Icons Services/project-management.png'
+import TA from '../Assets/Icons Services/start-up.png'
+import MPTI from '../Assets/Icons Services/file-managament.png'
 import { Link, useNavigate } from "react-router-dom"
+import Table from "../Component/TableMahasiswa/Table"
+import { useState } from "react"
 
 const MahasiswaPage = ({profile, home}) => {
 const navigate = useNavigate()
+const [services, setServices] = useState('MBKM')
+
+const handleClick = (item) => {
+    setServices(item)
+}
 
 const form = [
     {
         id: 1,
-        image : MBKM,
-        title : "Merdeka Belajar Kampus Merdeka - MBKM",
+        image : KP,
+        title : "Kerja Praktik",
         path : '/mahasiswa/form-MBKM'
 
     },
     {
         id: 2,
-        image: TA,
-        title: "Tugas Akhir",
+        image: MBKM,
+        title: "Merdeka Belajar Kampus Merdeka - MBKM",
         path : '/mahasiswa/form-tugas-akhir'
     },
     {
         id: 3,
-        image: MPTI,
-        title: "Manajemen Proyek Teknologi Informasi - MPTI",
+        image: TA,
+        title: "Tugas Akhir",
         path : '/mahasiswa/form-MPTI'
     },
     {
         id: 4,
-        image: TP,
-        title: "Kerja Praktik",
+        image: MPTI,
+        title: "Manajemen Project Teknologi Informasi - MPTI",
         path : '/mahasiswa/form-kerja-praktik'
-    }
+    },
 ]
+
+
 
 console.log(form.map(i => i.path))
 
@@ -45,27 +54,51 @@ const handleProfile = () => {
     return(
         <div className="w-full flex flex-col font-['Poppins']">
             <Navbar username={profile} home={home}/>
-            <div className="flex flex-col">
+            <div className="flex flex-col my-10">
                 {/* Nama dan identitas mahasiswa */}
-                <div className="relative mt-10 flex flex-col m-auto w-[97%] h-40 bg-[--primary-color] rounded-md text-white">
+                <div className="relative flex flex-col m-auto w-[97%] h-40 bg-[--primary-color] rounded-md text-white">
                     <h1 className="text-5xl font-bold ml-4 mt-4">Luthfie Arsya Darmaputra</h1>
                     <h3 className="text-md italic ml-4">Teknik Informatika</h3>
                     <button className="absolute right-4 bottom-2" onClick={handleProfile}>Check Details</button>
                 </div>
-                <h1 className="ml-4 font-bold text-3xl mt-4 text-[--primary-color]">Form Pengajuan</h1>
-                <div className="flex flex-row mx-4 w-[97%] mt-2 h-72 items-center justify-between">
+                <h1 className="ml-4 font-semibold text-3xl mt-10 text-[--primary-color]">Form Pengajuan</h1>
+               <div className="flex items-center w-full justify-center">
+                <div className="flex flex-row mt-2 w-[97%] h-56 items-center justify-center">
                     {form.map((items, index) => {
                         return(
-                                <Link key={items.id} to={items.path} className="w-[280px] bg-white flex flex-col justify-evenly items-center h-[280px] rounded-md shadow-[4px_4px_10px_-3px_black] hover:scale-110 hover:cursor-pointer transition-all transform hover:shadow-[6px_6px_12px_-10px_black] ">
+                            <div className="w-[80%] h-[100px] items-center m-auto flex flex-col justify-center">
+                            <div className="w-full flex items-center justify-center m-auto flex-col">
+                             <div className="w-[290px]  bg-white flex flex-col justify-center items-center h-[200px] rounded-md shadow-[4px_4px_10px_-3px_black] hover:scale-110 hover:cursor-pointer transition-all transform hover:shadow-[6px_6px_12px_-10px_black]">
                                     <img
                                     src={items.image}
                                     width={120}
                                     height={120}
                                     />
-                                    <h2 className="text-center mx-4 text-md font-semibold text-[--primary-color]">{items.title}</h2>
+                                <Link key={items.id} to={items.path} className="w-[250px] mt-2 h-[50px] flex items-center justify-center">
+                                    <h2 className="text-center text-md font-semibold text-[--primary-color]">{items.title}</h2>
                                 </Link>
+                             </div>
+                            </div>
+                    </div>
                         )
                     })}
+                </div>
+                </div> 
+                <div>
+                    <h1 className="ml-4 font-semibold text-3xl mt-10 text-[--primary-color]">Status Pengajuan</h1>
+                    <div className="w-[97%] m-auto border-b-2 border-gray-500 h-10">
+                    <div className="w-[40%] mt-6 h-10 flex justify-between  flex-row items-center text-gray-500 font-regular border-gray-500">
+                            {['MBKM', 'MPTI', 'Tugas Akhir', 'Kerja Praktik'].map((items) => {
+                                return(
+                                    <div className="flex flex-row text-center">
+                                        <button onClick={() => handleClick(items)} type="button"
+                                        className={`h-10 p-4 pb-1 relative  flex text-center items-center ${services === items ? `text-[--primary-color] border-b-2 border-[--primary-color]` : `border-gray-500`}`}    
+                                        >{items}</button>
+                                    </div>
+                                )
+                            })}
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
