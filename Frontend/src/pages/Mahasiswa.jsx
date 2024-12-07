47f55af54cf676c608abd705f4ccae686856918c
Lutfiarsya <lutfiarsya34@gmail.com>
2024-12-07 21:35:59 +0700

    revisi layout

--- Frontend/src/pages/Mahasiswa.jsx
+++ Frontend/src/pages/Mahasiswa.jsx
@@ -20,32 +20,104 @@ const form = [
         id: 1,
         image : KP,
         title : "Kerja Praktik",
-        path : '/mahasiswa/form-MBKM'
+        path : '/mahasiswa/form-kerja-praktik'
 
     },
     {
         id: 2,
         image: MBKM,
         title: "Merdeka Belajar Kampus Merdeka - MBKM",
-        path : '/mahasiswa/form-tugas-akhir'
+        path : '/mahasiswa/form-mbkm'
     },
     {
         id: 3,
         image: TA,
         title: "Tugas Akhir",
-        path : '/mahasiswa/form-MPTI'
+        path : '/mahasiswa/form-tugas-akhir'
     },
     {
         id: 4,
         image: MPTI,
         title: "Manajemen Project Teknologi Informasi - MPTI",
-        path : '/mahasiswa/form-kerja-praktik'
+        path : '/mahasiswa/form-mpti'
     },
 ]
 
 
+const data = {
+    'MBKM' : [
+        {
+            keterangan : 'Pendataan MBKM',
+            nama: "Luthfie Arsya",
+            Dosen: 'Dr Akmalll sanjaya Tr',
+            Status: 'Berhasil'
+        },
+        {
+            keterangan : 'Laporan Akhir MBKM',
+            nama: "Luthfie Arsya",
+            Dosen: 'Dr Akmalll sanjaya Tr',
+            Status: 'Berhasil'
+        }
+    ],
+    'TA' : [
+        {
+            keterangan : 'Pendataan Tugas Akhir',
+            nama: "Luthfie Arsya",
+            Dosen: 'Dr Akmalll sanjaya Tr',
+            Status: 'Berhasil'
+        },
+        {
+            keterangan : 'Pendaftaran Sidang Akhir',
+            nama: "Luthfie Arsya",
+            Dosen: 'Dr Akmalll sanjaya Tr',
+            Status: 'Berhasil'
+        }
+    ],
+    'KP' : [
+        {
+            keterangan : 'Peminatan Kerja Praktik',
+            nama: "Luthfie Arsya",
+            Dosen: 'Dr Akmalll sanjaya Tr',
+            Status: 'Berhasil'
+        },
+        {
+            keterangan : 'Pendaftaran Sidang Akhir',
+            nama: "Luthfie Arsya",
+            Dosen: 'Dr Akmalll sanjaya Tr',
+            Status: 'Berhasil'
+        },        {
+            keterangan : 'Pengumpulan Laporan Akhir',
+            nama: "Luthfie Arsya",
+            Dosen: 'Dr Akmalll sanjaya Tr',
+            Status: 'Berhasil'
+        }
+    ],
+    'MPTI' : [
+        {
+            keterangan : 'Pendaftaran MPTI',
+            nama: "Luthfie Arsya",
+            Dosen: 'Dr Akmalll sanjaya Tr',
+            Status: 'Berhasil'
+        },
+        {
+            keterangan : 'Peminatan MPTI',
+            nama: "Luthfie Arsya",
+            Dosen: 'Dr Akmalll sanjaya Tr',
+            Status: 'Berhasil'
+        },
+        {
+            keterangan : 'Pendaftaran Sempro',
+            nama: "Luthfie Arsya",
+            Dosen: 'Dr Akmalll sanjaya Tr',
+            Status: 'Berhasil'
+        }
+    ],
+}
 
-console.log(form.map(i => i.path))
+
+const selectedData = data[services]
+
+
 
 
 const handleProfile = () => {
@@ -67,18 +139,20 @@ const handleProfile = () => {
                     {form.map((items, index) => {
                         return(
                             <div className="w-[80%] h-[100px] items-center m-auto flex flex-col justify-center">
-                            <div className="w-full flex items-center justify-center m-auto flex-col">
-                             <div className="w-[290px]  bg-white flex flex-col justify-center items-center h-[200px] rounded-md shadow-[4px_4px_10px_-3px_black] hover:scale-110 hover:cursor-pointer transition-all transform hover:shadow-[6px_6px_12px_-10px_black]">
-                                    <img
-                                    src={items.image}
-                                    width={120}
-                                    height={120}
-                                    />
-                                <Link key={items.id} to={items.path} className="w-[250px] mt-2 h-[50px] flex items-center justify-center">
-                                    <h2 className="text-center text-md font-semibold text-[--primary-color]">{items.title}</h2>
+                                <Link key={items.id} to={items.path}>
+                                    <div className="w-full flex items-center justify-center m-auto flex-col">
+                                    <div className="w-[290px]  bg-white flex flex-col justify-center items-center h-[200px] rounded-md shadow-[4px_4px_10px_-3px_black] hover:scale-110 hover:cursor-pointer transition-all transform hover:shadow-[6px_6px_12px_-10px_black]">
+                                            <img
+                                            src={items.image}
+                                            width={120}
+                                            height={120}
+                                            />
+                                        <div className="w-[250px] mt-2 h-[50px] flex items-center justify-center">
+                                            <h2 className="text-center text-md font-semibold text-[--primary-color]">{items.title}</h2>
+                                        </div>
+                                    </div>
+                                    </div>
                                 </Link>
-                             </div>
-                            </div>
                     </div>
                         )
                     })}
@@ -99,6 +173,33 @@ const handleProfile = () => {
                             })}
                         </div>
                     </div>
+                    <div className="relative w-full max-h-[280px] overflow-y-auto border border-gray-300">
+                        <table className="w-full table-auto border-collapse  text-white text-center font-['Poppins'] shadow-[4px_4px_5px_-4px_black]">
+                            <thead>
+                                <tr className="bg-[--primary-color] h-12 sticky top-0 z-10 text-white">
+                                    <th className="border-x-1 border-white">Keterangan Program</th>
+                                    <th>Nama</th>
+                                    <th>Dosen Pembimbing</th>
+                                    <th>Status</th>
+                                    <th>Action</th>
+                                </tr>
+                    </thead>
+                    <tbody className="text-center">
+                    {selectedData?.map((items, index) => {
+                        return(
+                        <tr key={index} className="bg-white text-black h-12 border-t">
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                        </tr>
+                        )
+                    })}
+                </tbody>
+            </table>
+        </div>
                 </div>
             </div>
         </div>
