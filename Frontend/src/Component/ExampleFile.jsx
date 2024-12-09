bb9cd9be71b890dcafc633ab686db973d6fb213a
Lutfiarsya <lutfiarsya34@gmail.com>
2024-12-09 20:40:00 +0700

    Update preview File

--- Frontend/src/Component/ExampleFile.jsx
+++ Frontend/src/Component/ExampleFile.jsx
@@ -1,14 +1,14 @@
+import { X } from '@phosphor-icons/react'
 import file from '../Assets/Image3.jpg'
-const ExampleFile = () => {
+const ExampleFile = ({setClose}) => {
     return(
         <div className="fixed top-0 left-0 right-0 bottom-0 bg-black bg-opacity-50 z-20">
-        <div className="top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-[--primary-color] text-white flex flex-col font-['Poppins'] relative font-regular rounded-md w-[600px] h-[600px] z-30 p-4">
+        <div className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-[--primary-color] text-white flex flex-col font-['Poppins']  font-regular rounded-md w-[600px] h-[600px] z-30 p-4">
             <img src={file}
-            width={500}
-            height={400} 
-            className='object-contain'
+            className='object-fill w-full h-full'
             />
         </div>
+        <X size={32} onClick={() => setClose(false)} weight="bold" className='right-5 top-5 fixed hover:cursor-pointer'/>
         </div>
     )
 }
