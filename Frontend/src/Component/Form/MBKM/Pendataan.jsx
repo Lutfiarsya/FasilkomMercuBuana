faf29382aab83860e57d02675a35cb77f7b7558a
Lutfiarsya <lutfiarsya34@gmail.com>
2024-12-17 09:56:33 +0700

    Mengatur file yang melbihi 20 karakter

--- Frontend/src/Component/Form/MBKM/Pendataan.jsx
+++ Frontend/src/Component/Form/MBKM/Pendataan.jsx
@@ -7,6 +7,7 @@ const [inputNIM, setInputNIM] = useState('')
 const [number, setNumber] = useState('')
 const [exampleFile, setExampleFile] = useState(false)
 const [selectedFile, setSelectedFile] = useState({})
+const [program, setProgram] = useState('')
 
 
 const changeNIM = (e) => {
@@ -19,17 +20,25 @@ const changeNIM = (e) => {
   const handleFile = (event) => {
     // validasi target file yang ingin di upload
     const {id, files: uploadedFiles } = event.target
-    console.log(uploadedFiles)
-    console.log(id)
-
+    const maxLengthFile = 20
+    
     // jika ada data dalam array
-    if(uploadedFiles.length > 0){
+    if(uploadedFiles.length > 0){   
+
+        // mengubah apabila nama file lebih dari 20 karakter
+        const updatedFiles = Array.from(uploadedFiles).map((items) => {
+            if(items.name.length > maxLengthFile){
+                const updateNameFile = items.name.slice(0, maxLengthFile) + '...'
+                return{...items, name: updateNameFile}
+            }
+            return items
+        })
+
         setSelectedFile((prev) => ({
             ...prev,
-            [id]: uploadedFiles
+            [id]: updatedFiles
         }));
     }
-    console.log(selectedFile)
     
   }
 
@@ -43,7 +52,10 @@ const changeNIM = (e) => {
     if (/^\d*$/.test(numberMahasiswa)) {
         setNumber(numberMahasiswa);
     }
-  };  
+  };
+  
+
+
 
     return(
         <div className="w-[80%] my-10 font-['Poppins'] text-[--primary-color] bg-gray-200 rounded-md shadow-[2px_2px_7px_-4px_black] flex flex-col h-full pb-10 items-center">
@@ -65,20 +77,30 @@ const changeNIM = (e) => {
                 </div>
                 <div className="w-full relative">
                     <CaretDown size={20} className="absolute right-4 top-[10px]"/>
-                    <select className="w-full h-10 shadow-[0px_0px_8px_-6px_black] rounded-sm px-2">
-                        <option disabled selected className="text-gray-400 italic">Program MBKM</option>
-                        <option>MSIB</option>
-                        <option>Wirausaha Merdeka</option>
-                        <option>IISMA</option>
-                        <option>Kampus Mengajar</option>
-                        <option>Studi Independent</option>
-                        <option>Pertukaran Mahasiswa Merdeka</option>
+                    <select className="w-full h-10 shadow-[0px_0px_8px_-6px_black] rounded-sm px-2" onChange={(e) => {setProgram(e.target.value)}}>
+                        <option disabled selected className="text-gray-400 italic" >Program MBKM</option>
+                        <option value="MSIB">MSIB</option>
+                        <option value="Wirausaha">Wirausaha Merdeka</option>
+                        <option value="IISMA">IISMA</option>
+                        <option value="KampusMengajar">Kampus Mengajar</option>
+                        <option value="Studip">Studi Independent</option>
+                        <option value="Pertukaran">Pertukaran Mahasiswa Merdeka</option>
                     </select>
                 </div>
+                {program == 'Studip' ? 
+                    <select className="w-full h-10 shadow-[0px_0px_8px_-6px_black] rounded-sm px-2 mt-6">
+                        <option disabled selected className="text-gray-400 italic" >Kategori Mitra</option>
+                        <option value="MSIB">Bangkit</option>
+                        <option value="Wirausaha">MyEduSolve</option>
+                        <option value="IISMA">Hacktiv8</option>
+                    </select>
+                : 
+                
+                null }
                 <input type="text" id="Dosen" className="w-full h-10 my-6 shadow-[0px_0px_8px_-6px_black] rounded-sm  px-2" placeholder="Nama Dosen Pembimbing Akademik"/>
                 <input type="text" id="Intuisi" className="w-full h-10 shadow-[0px_0px_8px_-6px_black] rounded-sm  px-2" placeholder="Intuisi / Mitra Kolaborasi"/>
-                <input type="text" id="Deskripsi Kegiatan" className="w-full h-10 my-6 shadow-[0px_0px_8px_-6px_black] rounded-sm  px-2" placeholder="Nama Pembimbing MBKM"/>
-                <input type="text" id="Deskripsi Kegiatan" className="w-full h-10 shadow-[0px_0px_8px_-6px_black] rounded-sm  px-2" placeholder="Nama Pembimbing Industri"/>
+                <input type="text" id="NamaPembimbingMBKM" className="w-full h-10 my-6 shadow-[0px_0px_8px_-6px_black] rounded-sm  px-2" placeholder="Nama Pembimbing MBKM"/>
+                <input type="text" id="NamaPembimbingIndustri" className="w-full h-10 shadow-[0px_0px_8px_-6px_black] rounded-sm  px-2" placeholder="Nama Pembimbing Industri"/>
                 <form action="/upload" method="" enctype="multipart/form-data" className="w-full my-6 flex flex-col h-[200px] justify-between">
 
 
@@ -115,7 +137,7 @@ const changeNIM = (e) => {
                     <div className="w-full  flex flex-row">
                         <label className="w-[45%] h-10 relative text-gray-400 italic shadow-[0px_0px_8px_-6px_black] rounded-sm flex hover:cursor-pointer items-center justify-start bg-white px-2">
                         {selectedFile['Silabus'] ? <FileText size={25} color="#00376d" weight="fill"  className="mx-6"/> : <Upload size={25} className="mx-6" color="#00376d"/> }
-                        {selectedFile['Silabus'] ? selectedFile['Silabus'][0]?.name : `Upload Silabus MBKM`}
+                        {selectedFile['Silabus'] ? selectedFile['Silabus'][0]?.name : `Upload Silabus MBKM~~`}
                         <input onChange={handleFile} type="file" id="Silabus" className="w-[45%] hidden h-10 shadow-[0px_0px_8px_-6px_black] rounded-sm text-center bg-white px-2" placeholder="Upload Silabus MBKM"/>
                         </label>
                         {exampleFile ? 
@@ -139,7 +161,7 @@ const changeNIM = (e) => {
                         }
                     </div>
                 </form>
-                <input maxLength={13} type="text" id="No Handphone" value={number} onChange={changeNumber} className="w-full h-10 shadow-[0px_0px_8px_-6px_black] rounded-sm  px-2" placeholder="No handphone"/>
+                <input maxLength={13} type="text" id="NoHandphone" value={number} onChange={changeNumber} className="w-full h-10 shadow-[0px_0px_8px_-6px_black] rounded-sm  px-2" placeholder="No handphone"/>
                 <div className="w-full flex justify-end mt-6">
                 <button className="bg-[--primary-color] w-36 rounded-sm 0 my-6 h-10 text-white">Kirim</button>
                 </div>
