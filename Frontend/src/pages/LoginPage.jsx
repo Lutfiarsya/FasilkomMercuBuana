7838f6577e434ba67a81cb5e2653fbe5f6c93bd7
Lutfiarsya <lutfiarsya34@gmail.com>
2024-12-22 19:21:52 +0700

    add button register

--- Frontend/src/pages/LoginPage.jsx
+++ Frontend/src/pages/LoginPage.jsx
@@ -58,7 +58,8 @@ const handleClick = (event) => {
               <h1 className="text-6xl text-[--primary-color] font-semibold font-['Poppins']">Login</h1>
               <p className="text-gray-600  mt-4 italic text-[10px] font-regular font-['Poppins']">Silahkan login menggunakan NIM dan Password anda</p>
             </div>
-              <div className=" flex flex-col h-[45%]  justify-around relative">
+            <div className="w-full flex flex-col  items-center">
+              <div className=" flex flex-col h-[50%]  justify-around relative">
                 <div className="relative">
                     <User size={21} className="absolute top-[12px] left-3"/>
                     <input onKeyDown={handleKey} value={username} onChange={(e) => setUsername(e.target.value)} placeholder="NIM / Username" className="w-[400px] h-[40px] h-12 px-10 rounded-md placeholder-italic text-md font-['Poppins'] shadow-[4px_4px_8px_-4px_black]"/>
@@ -67,9 +68,14 @@ const handleClick = (event) => {
                     <Lock size={21} className="absolute top-[12px] left-3"/>
                     <input onKeyDown={handleKey} value={password} placeholder="Password" type="password" onChange={(e) => setPassword(e.target.value)}  className="w-[400px] h-12 px-10 rounded-md italic text-md font-['Poppins'] shadow-[4px_4px_8px_-4px_black]" />
                 </div>
-                <button className="italic text-xs font-['Poppins'] absolute bottom-0 text-[--secondary-color] right-0">forget password?</button>
+                <button className="italic text-xs font-['Poppins'] absolute h-1 bottom-0  text-[--secondary-color] right-0">forget password?</button>
               </div>
+              <div className="w-[70%] h-72 flex flex-col items-center justify-evenly">
               <button onClick={handleClick}  className="bg-[--secondary-color] w-[400px] text-2xl h-16  rounded-md text-white font-semibold font-['Poppins'] shadow-[4px_4px_8px_-4px_black]">Login</button>
+              <button onClick={(e) => navigate('/register')}  className="bg-white w-[400px] text-2xl h-16  rounded-md text-[--secondary-color] font-semibold font-['Poppins'] shadow-[2px_2px_8px_-4px_black]">Register</button>
+              </div>
+
+            </div>
            </div>
 
            {/* Slider */}
