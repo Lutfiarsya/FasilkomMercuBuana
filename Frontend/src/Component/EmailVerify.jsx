bc14a8c3a8dfe1822a0abfc8432d292b0e2afaa8
Lutfiarsya <lutfiarsya34@gmail.com>
2025-01-08 14:17:55 +0700

    add func tombol back

--- Frontend/src/Component/EmailVerify.jsx
+++ Frontend/src/Component/EmailVerify.jsx
@@ -1,12 +1,13 @@
-
+import { useNavigate } from 'react-router-dom'
 import logo from '../Assets/MercuIcons.png'
 import CountDown from '../Utilities/CountDown'
-const EmailVerify = ({valueGmail}) => {
-
-
-
-
+const EmailVerify = ({valueGmail, setVerifyGmail}) => {
 const mappingInput = Array.from({length: 6}, (_, i) => i + 1)
+const navigate = useNavigate()
+
+const handleBack = () => {
+    setVerifyGmail(true)
+}
 
 
 
@@ -38,7 +39,7 @@ const mappingInput = Array.from({length: 6}, (_, i) => i + 1)
                 })}
             </div>
             <div className='w-[80%] flex flex-row justify-between mt-4 p-4'>
-                <button className='w-44 bg-white text-[--primary-color] rounded-md shadow-sm shadow-[--primary-color] h-10'>Back</button>
+                <button className='w-44 bg-white text-[--primary-color] rounded-md shadow-sm shadow-[--primary-color] h-10' onClick={handleBack}>Back</button>
                 <button className='w-44 bg-[--primary-color] text-white rounded-md shadow-sm shadow-black h-10'>Verify Code</button>
             </div>
             <div className='w-[45%] mt-4 text-sm flex flex-row items-center justify-around'>
