40d33e908d8c2073f9df93791e350d1816d5ffff
Lutfiarsya <lutfiarsya34@gmail.com>
2025-01-08 08:46:37 +0700

    tinggal modify input

---
+++ Frontend/src/Component/EmailVerify.jsx
@@ -0,0 +1,53 @@
+import { useEffect, useState } from 'react'
+import logo from '../Assets/MercuIcons.png'
+import CountDown from '../Utilities/CountDown'
+const EmailVerify = ({valueGmail}) => {
+const [codeverify, setCodeVerify] = useState([])
+
+
+
+const mappingInput = Array.from({length: 6}, (_, i) => i + 1)
+
+
+
+    return(
+        <div className='flex flex-col w-full h-full items-center'>
+            <div className='mt-4 w-18 h-18 rounded-full bg-white border-gray- border-4 p-2'>
+             <img
+             src={logo}
+             width={50}
+             height={50}
+                />
+            </div>
+            <div className='flex flex-col w-full mt-4'>
+                <h1 className='font-semibold text-3xl'>Please Check Your Gmail</h1>
+                <p className="italic text-[12px]">we've send code to {valueGmail}</p>
+            </div>
+            <div className='flex flex-row items-center w-full justify-center'>
+                {mappingInput.map((index) => {
+                    return(
+                        <div className='flex flex-row w-14 mt-4 items-center justify-around'>
+                            <input
+                                key={index}
+                                className='border-2 border-gray-400 w-12 h-12 rounded-md text-center text-xl'
+                                maxLength={1}
+                                type='text'
+                            />
+                        </div>
+                    )
+                })}
+            </div>
+            <div className='w-[80%] flex flex-row justify-between mt-4 p-4'>
+                <button className='w-44 bg-white text-[--primary-color] rounded-md shadow-sm shadow-[--primary-color] h-10'>Back</button>
+                <button className='w-44 bg-[--primary-color] text-white rounded-md shadow-sm shadow-black h-10'>Verify Code</button>
+            </div>
+            <div className='w-[45%] mt-4 text-sm flex flex-row items-center justify-around'>
+                <p>Didn't recieve an email? </p>
+                <CountDown ResendGmail={60}/>
+            </div>
+        </div>
+    )
+}
+
+
+export default EmailVerify
