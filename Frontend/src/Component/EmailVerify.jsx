a1ded41d867b88cfae966b1b89e0b5fa651ccfdb
Lutfiarsya <lutfiarsya34@gmail.com>
2025-01-11 23:24:33 +0700

    modify send code to BE

--- Frontend/src/Component/EmailVerify.jsx
+++ Frontend/src/Component/EmailVerify.jsx
@@ -1,16 +1,37 @@
-import { useNavigate } from 'react-router-dom'
+import { useRef, useState } from 'react'
 import logo from '../Assets/MercuIcons.png'
 import CountDown from '../Utilities/CountDown'
-const EmailVerify = ({valueGmail, setVerifyGmail}) => {
-const mappingInput = Array.from({length: 6}, (_, i) => i + 1)
-const navigate = useNavigate()
+
+
+const EmailVerify = ({valueGmail, setVerifyGmail, code, validateCode}) => {
+const [input, setInput] = useState(['' , '', '','', '', ''])
+const nextInput = useRef([])
+
+
+
 
 const handleBack = () => {
-    setVerifyGmail(true)
+    setVerifyGmail('verify')
+}
+
+const handleVerifyCode = (index, value) => {
+    const newInputs = [...input]
+    setInput(newInputs)
+
+
+
+    if (value && index < input.length - 1) {
+        nextInput?.current[index + 1].focus();
+    }
+
 }
 
 
+const handleClick = () => {
+    const test = input.join('').toString()
+    setVerifyGmail('RegisterIdentity')
 
+}
     return(
         <div className='flex flex-col w-full h-full items-center'>
             <div className='mt-4 w-18 h-18 rounded-full bg-white border-gray- border-4 p-2'>
@@ -25,14 +46,16 @@ const handleBack = () => {
                 <p className="italic text-[12px]">we've send code to {valueGmail}</p>
             </div>
             <div className='flex flex-row items-center w-full justify-center'>
-                {mappingInput.map((index) => {
+                {input.map((items,index) => {
                     return(
-                        <div className='flex flex-row w-14 mt-4 items-center justify-around'>
-                            <input
-                                key={index}
+                        <div className='flex flex-row w-14 mt-4 items-center justify-around' key={index}>
+                            <input            
                                 className='border-2 border-gray-400 w-12 h-12 rounded-md text-center text-xl'
                                 maxLength={1}
-                                type='text'
+                                type='text' 
+                                onChange={(e) => handleVerifyCode(index, e.target.value)}
+                                ref={(e) => (nextInput.current[index] = e)}
+                                onInput={(e) => e.target.value =  e.target.value.replace(/[^0-9]/g, '')}
                             />
                         </div>
                     )
@@ -40,7 +63,7 @@ const handleBack = () => {
             </div>
             <div className='w-[80%] flex flex-row justify-between mt-4 p-4'>
                 <button className='w-44 bg-white text-[--primary-color] rounded-md shadow-sm shadow-[--primary-color] h-10' onClick={handleBack}>Back</button>
-                <button className='w-44 bg-[--primary-color] text-white rounded-md shadow-sm shadow-black h-10'>Verify Code</button>
+                <button className='w-44 bg-[--primary-color] text-white rounded-md shadow-sm shadow-black h-10' onClick={handleClick}>Verify Code</button>
             </div>
             <div className='w-[45%] mt-4 text-sm flex flex-row items-center justify-around'>
                 <p>Didn't recieve an email? </p>
