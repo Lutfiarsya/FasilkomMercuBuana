abefc627d15a3e3bcfa260dcdc50fcc776a6c161
Lutfiarsya <lutfiarsya34@gmail.com>
2025-01-18 21:09:23 +0700

    Hapus padding

--- Frontend/src/Component/ExampleFile.jsx
+++ Frontend/src/Component/ExampleFile.jsx
@@ -3,7 +3,7 @@ import file from '../Assets/Image3.jpg'
 const ExampleFile = ({setClose}) => {
     return(
         <div className="fixed top-0 left-0 right-0 bottom-0 bg-black bg-opacity-50 z-20">
-        <div className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-[--primary-color] text-white flex flex-col font-['Poppins']  font-regular rounded-md w-[600px] h-[600px] z-30 p-4">
+        <div className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-[--primary-color] text-white flex flex-col font-['Poppins']  font-regular rounded-md w-[600px] h-[600px] z-30">
             <img src={file}
             className='object-fill w-full h-full'
             />
