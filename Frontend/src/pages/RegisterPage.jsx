740fe5a6ebaccea3c2bc4808811de2dd7b72a22c
Lutfiarsya <lutfiarsya34@gmail.com>
2025-01-23 23:24:23 +0700

    change to dinamis button

--- Frontend/src/pages/RegisterPage.jsx
+++ Frontend/src/pages/RegisterPage.jsx
@@ -5,7 +5,8 @@ import RegistMahasiswa from "../Component/RegistMahasiswa"
 import logoMercu from '../Assets/MercuIcons.png'
 import ProgressTracking from "../Utilities/ProgressTracking"
 import RegistDosen from "../Component/RegistDosen"
-import { PrimaryButton } from "../Component/ButtonGroup"
+import { PrimaryButton, SecondaryButton } from "../Component/ButtonGroup"
+import { useNavigate } from "react-router-dom"
 
 const Register = () =>{
 const [gmailVerify, setGmailVerify] = useState('')
@@ -14,6 +15,7 @@ const [validateCode, setValidateCode] = useState([])
 const [fillSuccess, setFillSuccess] = useState(false)
 const emailMahasiswa = 'student.mercubuana.ac.id';
 const emailDosen = 'mercubuana.ac.id';
+const navigate = useNavigate()
 
 const handleVerify = () => {
     const pattern = new RegExp(`^[A-Za-z0-9\s@]+@${emailDosen}|[A-Za-z0-9\s@]+@${emailMahasiswa}`)
@@ -64,7 +66,7 @@ const slideVariantsExit = {
                          onChange={(e) => setGmailVerify(e.target.value)}
                          value={gmailVerify}
                         />
-                        <PrimaryButton functionButton={handleVerify} text={'Verify Gmail'} width={'80%'} height={'10'} style={'text-sm mt-4'}/>
+                        <PrimaryButton functionButton={handleVerify} text={'Verify Gmail'} width={'80%'} height={'10'} style={'text-sm mt-4 rounded-md'}/>
                         </div>
                         </motion.div>    
                     </motion.div> 
@@ -97,8 +99,8 @@ const slideVariantsExit = {
                             {gmailVerify.includes(emailMahasiswa) ?  <RegistMahasiswa additional={fillSuccess}/> : <RegistDosen additional={fillSuccess} />} 
                             </div>
                             <div className="w-full flex flex-row mt-2 justify-end">
-                                <button className="w-36 h-10 bg-white font-semibold rounded-md text-[--primary-color] mr-6 border-2 border-[--primary-color]" onClick={() => setFillSuccess(false)}>Back</button>
-                                <button className="w-36 h-10 text-white font-semibold rounded-md bg-[--primary-color] mr-6" onClick={() => setFillSuccess(true)}>{fillSuccess ? 'Submit' : 'Next'}</button>
+                            <SecondaryButton text={'Back'} functionButton={() => {fillSuccess ?  setFillSuccess(false) : setCodeVerify('verify')}}  style={'w-36 mr-6 border-2 px-4 border-[--primary-color]'}/>
+                            <PrimaryButton text={fillSuccess ? 'Submit' : 'Next'} functionButton={() =>  setFillSuccess(true)} style={'rounded-md mr-7 px-4 w-36'}  height={'10'}/>
                             </div>
                         </div>
                     }
