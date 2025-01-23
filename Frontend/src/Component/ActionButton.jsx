e3477d06e81ca5878542ea193b259c16b7809572
Lutfiarsya <lutfiarsya34@gmail.com>
2025-01-23 23:21:28 +0700

    change to dinamis button

--- Frontend/src/Component/ActionButton.jsx
+++ Frontend/src/Component/ActionButton.jsx
@@ -1,17 +1,13 @@
 import { Download, Eye, Trash } from "@phosphor-icons/react";
+import { PrimaryButton, ThirdButton, TrashButton } from "./ButtonGroup";
 
 const ActionButton = () => {
     return(
         <div className="flex w-[70%] h-10 flex-row m-auto items-center justify-around">
-            <button id="read" className="w-8 h-8 bg-[--primary-color] flex items-center justify-center rounded-lg shadow-[2px_2px_7px_-4px_black]">
-                <Eye color="#fff" size={15}/>
-            </button>
-            <button className="w-8 h-8 bg-[#CC2B52] flex items-center justify-center rounded-lg shadow-[2px_2px_7px_-4px_black]">
-                <Trash color="#fff" size={15}/>
-            </button>
-            <button className="w-8 h-8 bg-[#06D001] flex items-center justify-center rounded-lg shadow-[2px_2px_7px_-4px_black]">
-                <Download color="#fff" size={15}/>
-            </button>
+            <PrimaryButton width={'32px'} height={'8'} text={<Eye color="#fff" size={15}/>} style={'rounded-lg flex justify-center px-2 items-center'}/>
+            <TrashButton width={'32px'} height={'8'} text={<Trash color="#fff" size={15}/>} style={'rounded-lg flex justify-center px-2 items-center'}/>
+            <ThirdButton width={'32px'} height={'8'} text={<Download color="#fff" size={15}/>} style={'rounded-lg flex justify-center px-2 items-center'}/>
+                
         </div>
     )
 }
